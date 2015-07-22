cc1220884b87d8576c9ad02428edd7f8153e2034
Victor Milovanov <lostfreeman@gmail.com>
2015-07-21 23:38:12 -0700

    + APTCA.Annotations with SecuredForPartialTrustAttribute class

---
+++ src/APTCA.Annotations/SecuredForPartialTrustAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace APTCA.Annotations
+{
+    using System;
+    using static System.AttributeTargets;
+
+    /// <summary>
+    /// Indicates, that specified member has been properly secured for partially trusted callers
+    /// </summary>
+    [AttributeUsage(validOn: Constructor | Method | Property | Event, AllowMultiple = false, Inherited = false)]
+    public class SecuredForPartialTrustAttribute: Attribute{}
+}
